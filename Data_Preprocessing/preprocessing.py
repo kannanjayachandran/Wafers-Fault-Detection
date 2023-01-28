986fe3772a26e8394e76d219e57be9051ed40c04
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2023-01-28 20:00:44 +0530

    completed preprocessing.py file

--- Data_Preprocessing/preprocessing.py
+++ Data_Preprocessing/preprocessing.py
@@ -93,12 +93,66 @@ class Preprocessor:
             return self.null_present
 
         except Exception as e:
-            self.logger_object.log(self.file_object, 'Exception occured in is_null_present method of the Preprocessor '
+            self.logger_object.log(self.file_object, 'Exception occurred in is_null_present method of the Preprocessor '
                                                      'class. Exception message: ' + str(e))
             self.logger_object.log(self.file_object, 'Finding missing values failed. Exited the is_null_present '
                                                      'method of the Preprocessor class')
             raise Exception()
 
     def impute_missing_values(self, data):
+        """
+        Method Name: impute_missing_values
+        Description: This method replaces all the missing values in the Dataframe using KNN Imputer.
+        Output: A Dataframe which has all the missing values imputed.
+        :param data:
+        :return:
+        """
 
-        return None
+        self.logger_object.log(self.file_object, 'Entered the impute_missing_values method of the Preprocessor class')
+        self.data = data
+        try:
+            imputer = KNNImputer(n_neighbors=3, weights='uniform', missing_values=np.nan)
+            self.new_array = imputer.fit_transform(self.data)
+
+            # nd array -> dataframe
+            self.new_data = pd.DataFrame(data=self.new_array, columns=self.data.columns)
+            self.logger_object.log(self.file_object, 'Successfully imputed missing values. Exited the ' 
+                                                     'impute_missing_values method of the Preprocessor class')
+            return self.new_data
+
+        except Exception as e:
+            self.logger_object.log(self.file_object, 'Exception occurred in impute_missing_values method of the'
+                                                    ' Preprocessor class. Exception message:  ' + str(e))
+            self.logger_object.log(self.file_object, 'Imputing missing values failed. Exited the impute_missing_values '
+                                                    'method of the Preprocessor class')
+            raise Exception()
+
+    def get_columns_with_zero_std_deviation(self, data):
+        """
+        Method Name: get_columns_with_zero_std_deviation
+        Description: This method finds out the columns which have a standard deviation of zero.
+        Output: List of the columns with standard deviation of zero
+        :param data:
+        :return:
+        """
+        self.logger_object.log(self.file_object, 'Entered the get_columns_with_zero_std_deviation method '
+                                                 'of the Preprocessor class')
+        self.columns = data.columns
+        self.data_n = data.describe()
+        self.col_to_drop = []
+        try:
+            for x in self.columns:
+                if self.data_n[x]['std'] == 0:  # check if standard deviation is zero
+                    self.col_to_drop.append(x)  # prepare the list of columns with standard deviation zero
+            self.logger_object.log(self.file_object, 'Column search for Standard Deviation of Zero Successful. '
+                                                     'Exited the get_columns_with_zero_std_deviation method of '
+                                                     'the Preprocessor class')
+            return self.col_to_drop
+
+        except Exception as e:
+            self.logger_object.log(self.file_object,'Exception occurred in get_columns_with_zero_std_deviation method '
+                                                    'of the Preprocessor class. Exception message:  ' + str(e))
+            self.logger_object.log(self.file_object, 'Column search for Standard Deviation of Zero Failed. Exited '
+                                                     'the get_columns_with_zero_std_deviation method of the '
+                                                     'Preprocessor class')
+            raise Exception()
