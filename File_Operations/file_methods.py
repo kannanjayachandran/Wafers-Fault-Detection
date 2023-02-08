7498855db7739a1767694f523bf3055e421d37f7
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2023-02-08 18:27:03 +0530

    completed file_methods.py file

--- File_Operations/file_methods.py
+++ File_Operations/file_methods.py
@@ -44,3 +44,58 @@ class FileOperation:
                                    'Model File ' + filename + ' could not be saved. Exited the save_model method '
                                                               'of the Model_Finder class')
             raise Exception()
+
+    def load_model(self, filename):
+        """
+            Method Name: load_model
+            Description: load the model file to memory
+            Output: The Model file loaded in memory
+            On Failure: Raise Exception
+        """
+        self.logger_object.log(self.file_object, 'Entered the load_model method of the File_Operation class')
+        try:
+            with open(self.model_dir + filename + '/' + filename + '.sav', 'rb') as f:
+                self.logger_object.log(self.file_object, 'Model File ' + filename + ' loaded. Exited the load_model '
+                                                                                    'method of the Model_Finder class')
+                return pickle.load(f)
+
+        except Exception as e:
+            self.logger_object.log(self.file_object,
+                                   'Exception occurred in load_model method of the Model_Finder class. '
+                                   'Exception message:  ' + str(e))
+            self.logger_object.log(self.file_object,
+                                   'Model File ' + filename + ' could not be saved. Exited the load_model method of '
+                                                              'the Model_Finder class')
+            raise Exception()
+
+    def find_correct_model_file(self, cluster_number):
+        """
+            Method Name: find_correct_model_file
+            Description: Select the correct model based on cluster number
+            Output: The Model file
+            On Failure: Raise Exception
+        """
+        self.logger_object.log(self.file_object,
+                               'Entered the find_correct_model_file method of the File_Operation class')
+        try:
+            self.cluster_number = cluster_number
+            self.folder_name = self.model_dir
+            self.list_of_model_files = []
+            self.list_of_files = os.listdir(self.folder_name)
+            for self.file in self.list_of_files:
+                try:
+                    if (self.file_object.index(str(self.cluster_number)) != -1):
+                        self.model_name = self.file_object
+                except:
+                    continue
+            self.model_name = self.model_name.split('.')[0]
+            self.logger_object.log(self.file_object,
+                                   'Exited the find_correct_model_file method of the Model_Finder class.')
+            return self.model_name
+        except Exception as e:
+            self.logger_object.log(self.file_object,
+                                   'Exception occured in find_correct_model_file method of the Model_Finder class. '
+                                   'Exception message:  ' + str(e))
+            self.logger_object.log(self.file_object,
+                                   'Exited the find_correct_model_file method of the Model_Finder class with Failure')
+            raise Exception()
