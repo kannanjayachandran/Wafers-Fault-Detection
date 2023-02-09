c367c9a2ddad5fdc8e23cd8e53b250c45e58a86e
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2023-02-09 23:57:06 +0530

    created main file

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello world")
+from wsgiref import simple_server
+from flask_cors import CORS, cross_origin
+from flask import Flask, request, render_template, Response
+import json
+import os
+from Prediction_validation import PredictionValidation
+from Training_Model import TrainModel
+from Training_Validation_Insertion import Train_Validation
+from Prediction_model import Prediction
+
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
+
+app = Flask(__name__)
+CORS(app)
+
